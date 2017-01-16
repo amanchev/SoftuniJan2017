8c41ac7dbe1c83e00fc4e7c52806d2afbe8b9f5a
askata992 <askata9213@gmail.com>
2017-01-16 21:23:04 +0200

    upload 01.FirststepsHomework

---
+++ 01.FirstStepsInProgramming/06.SquareFromStars/Square.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Square
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0}", new string('*',n));
+
+        for (int i = 0; i < n -2; i++)
+        {
+            Console.WriteLine("*{0}*", new string(' ', n - 2));
+        }
+
+
+
+        Console.WriteLine("{0}", new string('*',n));
+    }
+}
---
+++ 01.FirstStepsInProgramming/ConsoleApplication1/ReactangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ReactangleArea
+{
+    static void Main(string[] args)
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double area = (a * b);
+        Console.WriteLine(area);
+
+
+    }
+}
+
